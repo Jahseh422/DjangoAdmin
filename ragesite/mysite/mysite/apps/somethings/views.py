384da560206ddfeddea15ff77a5507d5a43256f7
rage <fernando.petrenko@gmail.com>
2022-06-26 14:19:35 +0300

    shablonizator

--- ragesite/mysite/mysite/apps/somethings/views.py
+++ ragesite/mysite/mysite/apps/somethings/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Здарова!")
+    return render(request, 'somethings/list.html')
